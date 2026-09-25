e61dc066e45fda91462c3ec4089978587a3415a6
IlliaKrupko <krupkoilla397@gmail.com>
2026-09-26 01:12:44 +0200

    feat(user): add user service

--- src/main/java/krupkoillia/chesstracker/userservice/service/UserService.java
+++ src/main/java/krupkoillia/chesstracker/userservice/service/UserService.java
@@ -1,4 +1,43 @@
 package krupkoillia.chesstracker.userservice.service;
 
+import jakarta.persistence.EntityNotFoundException;
+import krupkoillia.chesstracker.userservice.dto.ChangePasswordRequestDto;
+import krupkoillia.chesstracker.userservice.dto.UserResponseDto;
+import krupkoillia.chesstracker.userservice.mapper.UserMapper;
+import krupkoillia.chesstracker.userservice.model.User;
+import krupkoillia.chesstracker.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    private final UserMapper userMapper;
+
+    public UserResponseDto getMe() {
+        // TODO: method logic
+        return null;
+    }
+
+    @Transactional
+    public UserResponseDto changeUsername(String username) {
+        // TODO: method logic
+        return null;
+    }
+
+    @Transactional
+    public void changePassword(ChangePasswordRequestDto requestDto) {
+        // TODO: method logic
+    }
+
+    public void delete() {
+        // TODO: method logic
+    }
+
 }
